c897b6d4da7a9f1739c2727d2946d1fa05260023
scholtde <dewald@rc.technology>
2019-12-16 11:30:36 +0200

    - Added files

--- dlora.py
+++ dlora.py
@@ -82,8 +82,8 @@ class Dlora:
         # Known UDID list
         self.known_UDID = ["0212233445566778899aabbccddeeff1"]
         self.camera_ai.known_UDID = self.known_UDID
-        if self.ble_scanner is not None:
-            self.camera_ai.start_ble_loop()
+        # if self.ble_scanner is not None:
+        #     self.camera_ai.start_ble_loop()
 
     def ble_services(self):
         # BLE scanner
