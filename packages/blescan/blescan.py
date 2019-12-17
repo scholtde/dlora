56216923ac1e2296f0bec320c74fb074dd8fe187
scholtde <dewald@rc.technology>
2019-12-17 13:53:29 +0200

    - Added IoThings features

--- packages/blescan/blescan.py
+++ packages/blescan/blescan.py
@@ -145,7 +145,7 @@ class bleScan:
         bluez.hci_filter_all_events(flt)
         bluez.hci_filter_set_ptype(flt, bluez.HCI_EVENT_PKT)
         # Set a timeout in order for pasing time out if nothing on socket is received
-        sock.settimeout(1)
+        sock.settimeout(0.5)
         # Set socket options
         sock.setsockopt(bluez.SOL_HCI, bluez.HCI_FILTER, flt)
 
