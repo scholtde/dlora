fd8ea3fbd8581cdeaefddce9f5b74723dded652d
scholtde <dewald@rc.technology>
2019-12-17 14:11:23 +0200

    - Added IoThings features

--- packages/ai_stream/ai_stream.py
+++ packages/ai_stream/ai_stream.py
@@ -135,8 +135,6 @@ class aiStreamer:
                                         self.ble_known_things[i]["object_classification"]].append(
                                         self.ble_known_things[i]["Details"])
 
-            print(self.dlora_class_vs_device)
-
     def update(self):
         # Read frame from the stream
         ret, self.frame = self.capture.read()
@@ -240,6 +238,7 @@ class aiStreamer:
                     dlora_label = "unknown"
                     if found_object in self.dlora_class_vs_device:
                         dlora_label = str(self.dlora_class_vs_device[found_object])
+                    print(dlora_label)
 
                     """if self.CLASSES[idx] != object:
                       # Skip rest of the statements if the object is not defined
